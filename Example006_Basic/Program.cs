d9daa1e478f2ff66ea202957b1e7a8a66fba8823
Anastasiya Kataeva <anastasiya.kataev@yandex.ru>
2023-06-08 23:29:58 +0300

    Add Example006_Basic

---
+++ Example006_Basic/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 5;
+int b = 3;
+int c = 9;
+int d = 1;
+int e = 8;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("Максимальное значение " + max);
