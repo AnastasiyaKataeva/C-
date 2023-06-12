4f60d0b8bacca64d1c6985a268e2ebc3a3384934
Anastasiya Kataeva <anastasiya.kataev@yandex.ru>
2023-06-12 21:24:16 +0300

    Seminar1_lesson3

---
+++ Seminar1_lesson3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+string inputedNum = Console.ReadLine();
+int number = Convert.ToInt32(inputedNum);
+if (number % 2 == 1) {
+    Console.WriteLine("Число " + number + " нечётное");
+} else {
+    Console.WriteLine("Число " + number + " чётное");
+};
