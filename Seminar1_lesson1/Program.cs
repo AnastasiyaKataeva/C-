16a8eb122f58f651b12f9a008a67e86703dc9bcd
Anastasiya Kataeva <anastasiya.kataev@yandex.ru>
2023-06-12 20:23:42 +0300

    Seminar1_lesson1

---
+++ Seminar1_lesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число ");
+string inputedFirstNum = Console.ReadLine();
+int firstNumber = Convert.ToInt32(inputedFirstNum);
+Console.Write("Введите второе число ");
+string inputedSecondNum = Console.ReadLine();
+int secondNumber = Convert.ToInt32(inputedSecondNum);
+
+if (firstNumber > secondNumber) {
+    Console.Write($"Число {firstNumber} больше чем {secondNumber}");
+} else {
+    Console.Write($"Число {secondNumber} больше чем {firstNumber}");
+};
