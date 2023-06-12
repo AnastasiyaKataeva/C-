fab0dcd59f6b7e8ea656fabd9bc0fdde4e79fa7c
Anastasiya Kataeva <anastasiya.kataev@yandex.ru>
2023-06-12 21:10:53 +0300

    Seminar1_lesson2

---
+++ Seminar1_lesson2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число ");
+string inputedFirstNum = Console.ReadLine();
+int firstNumber = Convert.ToInt32(inputedFirstNum);
+Console.Write("Введите второе число ");
+string inputedSecondNum = Console.ReadLine();
+int secondNumber = Convert.ToInt32(inputedSecondNum);
+Console.Write("Введите третье число ");
+string inputedThirdNum = Console.ReadLine();
+int thirdNumber = Convert.ToInt32(inputedThirdNum);
+
+int max = firstNumber;
+
+if (secondNumber > max) {
+     max = secondNumber;
+};
+if (thirdNumber > max) {
+     max = thirdNumber;
+};
+Console.Write($"Число {max} наибольшее");
